ba0ec7fb4a380984dd6bcc7f973bab4ff8d9760b
DaviBMMGallo <32436536+Lhimbo@users.noreply.github.com>
2021-12-01 17:29:31 -0300

    teste.html

--- python/Launcher.py
+++ python/Launcher.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__,template_folder=os.getcwd() +'\\Html')
+app = Flask(__name__,template_folder=os.getcwd() + "\\Html",static_folder=os.getcwd() + "\\static")
 
 @app.route("/")
 def homepage():
     return (
-        render_template('index.html')
+        render_template('test.html')
     ) 
 if __name__ == '__main__':
     app.run(debug=True)
